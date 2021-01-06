df82d19eb889d9bcca3379214dfaf7d95a32b813
Raghul Nanth <nanth.raghul@gmail.com>
2021-01-07 00:01:27 +0530

    Persistence in popup

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ chrome.runtime.onMessage.addListener(
       const done = message['done'];
       const url = window.location.href;
       const title = document.title.split('/').slice(1,-1).join('-');
+      const empty = message['empty'];
+      const val = message['val'];
       if(message.type === "addData") {
         chrome.storage.sync.get(['ctftime'], function(items) {
-        if(Object.keys(items).length === 0 && items.constructor === Object) {
+        if(empty) {
         let m = [{'url': url, 'title': title, 'done': done, 'difficulty': difficulty}];
         chrome.storage.sync.set({'ctftime': m});
         
       } else {
         let m = items['ctftime'];
-        const val = m.findIndex(data => data.url === url);
-        console.log(val);
         if(val === -1) {
           m.push({'url': url, 'difficulty': difficulty, 'done': done, 'title': title})
           chrome.storage.sync.set({'ctftime': m});
@@ -23,7 +23,6 @@ chrome.runtime.onMessage.addListener(
           chrome.storage.sync.set({'ctftime': m})
         }
       }
-      console.log(items['ctftime']);
     });  
   }
   return true;
