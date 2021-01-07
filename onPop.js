ab5076d596dd9d48ce67087fcfd47c90ee3c643e
Senthilnathan <mailsenthilnathan2001@gmail.com>
2021-01-07 15:13:17 +0530

    Add Datefinished

--- onPop.js
+++ onPop.js
@@ -21,11 +21,42 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   })
   button.addEventListener('click', function() {
   const completed = document.getElementById('done').checked;
+  if(completed)
+    timeCompleted = getDateTime();
+  else
+    timeCompleted = '----';
   let difficulty = false;
   if(document.querySelector('.container input:checked'))
     difficulty = document.querySelector('.container input:checked').value;
-    chrome.tabs.sendMessage(tabs[0].id, {'type': "addData", 'empty': empty, 'done': completed, 'val': val, 'difficulty': difficulty});
+    chrome.tabs.sendMessage(tabs[0].id, {'type': "addData", 'empty': empty, 'done': completed, 'val': val, 'difficulty': difficulty, 'timeCompleted': timeCompleted});
     window.location.reload();
   });
   return true;
 });
+
+function getDateTime() {
+  var now     = new Date(); 
+  var year    = now.getFullYear();
+  var month   = now.getMonth()+1; 
+  var day     = now.getDate();
+  var hour    = now.getHours();
+  var minute  = now.getMinutes();
+  var second  = now.getSeconds(); 
+  if(month.toString().length == 1) {
+       month = '0'+month;
+  }
+  if(day.toString().length == 1) {
+       day = '0'+day;
+  }   
+  if(hour.toString().length == 1) {
+       hour = '0'+hour;
+  }
+  if(minute.toString().length == 1) {
+       minute = '0'+minute;
+  }
+  if(second.toString().length == 1) {
+       second = '0'+second;
+  }   
+  var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
+   return dateTime;
+}
--- script.js
+++ script.js
@@ -6,20 +6,22 @@ chrome.runtime.onMessage.addListener(
       const title = document.title.split('/').slice(1,-1).join('-');
       const empty = message['empty'];
       const val = message['val'];
+      let timeCompleted = message['timeCompleted'];
       if(message.type === "addData") {
         chrome.storage.sync.get(['ctftime'], function(items) {
         if(empty) {
-        let m = [{'url': url, 'title': title, 'done': done, 'difficulty': difficulty}];
+        let m = [{'url': url, 'title': title, 'done': done, 'difficulty': difficulty, 'timeCompleted': timeCompleted}];
         chrome.storage.sync.set({'ctftime': m});
-        
       } else {
         let m = items['ctftime'];
         if(val === -1) {
-          m.push({'url': url, 'difficulty': difficulty, 'done': done, 'title': title})
+          m.push({'url': url, 'difficulty': difficulty, 'done': done, 'title': title, 'timeCompleted': timeCompleted})
           chrome.storage.sync.set({'ctftime': m});
         }
         else {
-          m[val] = {'url': url, 'difficulty': difficulty, 'done': done, 'title': title}
+          if(done == m[val].done)
+            timeCompleted =  m[val].timeCompleted;
+          m[val] = {'url': url, 'difficulty': difficulty, 'done': done, 'title': title, 'timeCompleted': timeCompleted}
           chrome.storage.sync.set({'ctftime': m})
         }
       }
